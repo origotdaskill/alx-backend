151fd4aed6e09916e094103b72a4a21e61d0e88f
origotdaskill <mr.walid.ori@gmaill.com>
2024-08-20 16:01:25 +0100

    ori commit

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
 """
-Basic ditionary
+FIFO Caching
 """
-
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """
-    Basic Cache class that inherits from BaseCaching
+    a class FIFOCache that inherits from BaseCaching and is a caching system
     """
+
+    def __init__(self):
+        """
+        Init method
+        """
+        super().__init__()
+        self.key_indexes = []
 
     def put(self, key, item):
         """
-        Must assign to the dictionary self.cache_data
+        assign to the dictionary self.cache_data
         the item value for the key key.
         """
         if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_discarded = self.key_indexes.pop(0)
+                del self.cache_data[item_discarded]
+                print("DISCARD:", item_discarded)
+
             self.cache_data[key] = item
+            self.key_indexes.append(key)
 
     def get(self, key):
         """
-        Must return the value in self.cache_data linked to key.
+        return the value in self.cache_data linked to key.
         """
-        return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
